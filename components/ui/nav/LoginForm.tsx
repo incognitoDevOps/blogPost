9e2fd67f1676bab241b4b7b0a290ca8a35f84542
incognitoDevOps <in110005620@kisiiuniversity.ac.ke>
2024-02-10 03:18:00 +0300

    login implimented

--- components/ui/nav/LoginForm.tsx
+++ components/ui/nav/LoginForm.tsx
@@ -4,16 +4,27 @@ import React from "react";
 import { Button } from "../button";
 import { FaGithub } from "react-icons/fa";
 import { createBrowserClient } from "@supabase/ssr";
+import { usePathname } from "next/navigation";
 
 export default function LoginForm() {
+  const pathname = usePathname()
   const supabase = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+  const handLogin = () =>{
+    supabase.auth.signInWithOAuth({
+      provider:"github",
+      options:{
+        redirectTo:location.origin + "/auth/callback?next="+ pathname,
+      }
+    })
+  }
   return (
-    <Button variant="outline" className="flex items-center gap-2">
-      {" "}
-      <FaGithub /> Login
+
+    <Button variant="outline" className="flex items-center gap-2" onClick={handLogin}>
+      <FaGithub />
+       Signin
     </Button>
   );
 }
